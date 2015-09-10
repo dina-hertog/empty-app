a6866dec09c9853fade570e4297578d27a326ba3
dina-hertog <dinahertog-raz@hanovernorwichschools.org>
2015-09-10 14:56:56 -0400

    Updated from Brython Server: 9/10/2015 2:56:56 PM

--- tutorial1.py
+++ tutorial1.py
@@ -5,14 +5,19 @@ red= Color(0xff0000, 1.0)
 green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
+white = Color(0xffffff, 1.0)
 
 thinline = LineStyle(1, black)
+thickline = LineStyle(4, black)
 
-rectangle1 = RectangleAsset(50, 20, thinline, blue)
-rectangle2 = RectangleAsset(100,40, thinline,red)
+rectangle1 = RectangleAsset(100, 100, thinline, blue)
+rectangle2 = RectangleAsset(100, 100, thinline, white)
+ellipse = EllipseAsset(10, 10, thickline, blue)
 
-Sprite(rectangle1, (225,210))
-Sprite(rectangle2, (200, 200))
+
+Sprite(rectangle1, (200,200))
+Sprite(rectangle2, (250, 250))
+Sprite(ellipse,(500, 500))
 
 myapp = App()
 myapp.run()
