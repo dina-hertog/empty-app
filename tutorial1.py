177aef1aea3f5f6bff0a679ffec259bcf0207f40
dina-hertog <dinahertog-raz@hanovernorwichschools.org>
2015-09-11 14:26:32 -0400

    Updated from Brython Server: 9/11/2015 2:26:32 PM

--- tutorial1.py
+++ tutorial1.py
@@ -2,7 +2,7 @@ from ggame import App, Color, LineStyle, Sprite
 from ggame import RectangleAsset, EllipseAsset, PolygonAsset
 
 red= Color(0xff0000, 1.0)
-green = Color(0x00ff00, 1.0)
+green = Color(0x00ff00, 0.5)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 white = Color(0xffffff, 1.0)
